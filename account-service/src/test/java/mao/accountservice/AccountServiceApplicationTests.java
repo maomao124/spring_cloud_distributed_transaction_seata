a4673246e9ea94dda20d00a6ffe6a657b93f9c5b
mao <1296193245@qq.com>
2022-07-24 20:20:51 +0800

    spring_cloud_distributed_transaction_seata

---
+++ account-service/src/test/java/mao/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
---
+++ storage-service/src/test/java/mao/storageservice/StorageServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.storageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
